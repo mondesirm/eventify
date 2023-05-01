31c808a7ec9e8fff779842b0b52faca0a4716f57
MONDESIR Malik <malikmondesir@gmail.com>
2023-05-01 12:38:29 +0200

    refacto splash screen

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import { Asset } from 'expo-asset'
 import Constants from 'expo-constants'
-import * as Updates from 'expo-updates'
 import { StatusBar } from 'expo-status-bar'
 import Toast from 'react-native-toast-message'
 import * as SplashScreen from 'expo-splash-screen'
 import { enableScreens } from 'react-native-screens'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
-import { Animated, Button, Platform, StyleSheet, Text, View } from 'react-native'
 import { adaptNavigationTheme, Provider as PaperProvider, useTheme } from 'react-native-paper'
 
+import Splash from '@/components/Splash'
 import Loading from '@/components/Loading'
 import NavigationProvider from '@/navigation'
 // import AppRouter from '@/components/AppRouter'
@@ -23,70 +20,6 @@ SplashScreen.preventAutoHideAsync().catch(() => {
 
 enableScreens()
 
-function AnimatedSplashScreen({ children, image }) {
-	const animation = useMemo(() => new Animated.Value(1), [])
-	const [isAppReady, setAppReady] = useState(false)
-	const [isSplashAnimationComplete, setAnimationComplete] = useState(false)
-
-	useEffect(() => {
-		if (isAppReady) {
-			Animated.timing(animation, {
-				toValue: 0,
-				duration: 1000,
-				useNativeDriver: true
-			}).start(() => setAnimationComplete(true))
-		}
-	}, [isAppReady])
-
-	const onImageLoaded = useCallback(async () => {
-		try {
-			await SplashScreen.hideAsync()
-			// Load stuff
-			await Promise.all([])
-		} catch (e) {
-		} finally { setAppReady(true) }
-	}, [])
-
-	return (
-		<View style={{ flex: 1 }}>
-			{isAppReady && children}
-
-			{!isSplashAnimationComplete &&
-				<Animated.View pointerEvents="none" style={[StyleSheet.absoluteFill, { backgroundColor: Constants.manifest.splash.backgroundColor, opacity: animation }]}>
-					<Animated.Image
-						style={{
-							width: '100%',
-							height: '100%',
-							resizeMode: Constants.manifest.splash.resizeMode || 'contain',
-							transform: [{ scale: animation }]
-						}}
-						source={image}
-						onLoadEnd={onImageLoaded}
-						fadeDuration={0}
-					/>
-				</Animated.View>
-			}
-		</View>
-	)
-}
-
-function AnimatedAppLoader({ children, image }) {
-	const [isSplashReady, setSplashReady] = useState(false)
-
-	useEffect(() => {
-		async function prepare() {
-			await Asset.fromURI(image.uri).downloadAsync()
-			setSplashReady(true)
-		}
-
-		prepare()
-	}, [image])
-
-	if (!isSplashReady) return null
-
-	return <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>
-}
-
 export default function App() {
 	const theme = useTheme() //? https://callstack.github.io/react-native-paper/docs/guides/theming/#extending-the-theme
 	const { LightTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme })
@@ -95,14 +28,14 @@ export default function App() {
 		<SafeAreaProvider>
 			<PreferencesContextProvider>
 				<PaperProvider theme={theme}>
-					<AnimatedAppLoader image={{ uri: Constants.manifest.splash.image }}>
+					<Splash image={{ uri: Constants.manifest.splash.image }}>
 						<StatusBar /* style={theme.dark ? 'light' : 'dark'} backgroundColor={theme.colors.primary} */ />
 						<NavigationContainer theme={LightTheme}>
-							<NavigationProvider />
 							<Loading />
+							<NavigationProvider />
 							<Toast />
 						</NavigationContainer>
-					</AnimatedAppLoader>
+					</Splash>
 				</PaperProvider>
 			</PreferencesContextProvider>
 		</SafeAreaProvider>
